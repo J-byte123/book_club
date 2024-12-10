d239f4e3d5c555e04ae66b7cc28a6b550a41ee93
Nicole Riera <150852807+NicoRi5@users.noreply.github.com>
2024-12-10 10:54:54 -0500

    Fix issue

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,38 +1,17 @@
 /* TODO - add your code to create a functional React component that renders account details for a logged in user. Fetch the account data from the provided API. You may consider conditionally rendering a message for other users that prompts them to log in or create an account.  */
 
 import { useEffect, useState } from "react";
-// import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
 
 export default function AccountInfo({ setToken }) {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  
   const [books, setBooks] = useState("");
   const [error, setError] = useState(null);
-  const [Success, setSuccess] = useState(null);
-
-
-
-async function handleSubmit(event) {
-    event.preventDefault();
-
-    if (!username || !email) {
-        setError('Please enter your username or email, followed by your password.');
-    }
 }
 
-try {
-    const response = await fetch("https://fsa-book-buddy-b6e748d1380d.herokuapp.com/docs/", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ username, email, password }),
-});
-
-const data = await response.json();
-
-
-  }
-};
+return (
+    <div>
+        <h2>Welcome {user.email}</h2>
+    </div>
+)
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,1 @@
 /* TODO - add your code to create a functional React component that renders a registration form */
-
-function registration() {
-  return <h2>Registration Forms</h2>;
-}
-
-// eslint-disable-next-line no-undef
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<registration form />);
-
-export default registration;
