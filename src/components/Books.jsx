5da9b59b4e6406073626cdbdb812b9a8dc2ba18c
MarsMig <mars.n.m@outlook.com>
2024-12-12 11:11:36 -0500

    first attempt at redux format for books.js and singlebook.js

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,40 +1,28 @@
 /* TODO - add your code to create a functional React component that displays all of the available books in the library's catalog. Fetch the book data from the provided API. Users should be able to click on an individual book to navigate to the SingleBook component and view its details. */
 
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { useParams } from "react";
+import { useGetBooksQuery } from "./apiSlice.js";
 
 export default BookList;
 function BookList() {
-  const [books, setBooks] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const navigate = useParams();
+  const { data } = useGetBooksQuery();
+  const books = data.books;
 
-  useEffect(() => {
-    const getBooks = async () => {
-      try {
-        const response = await axios.get(
-          "https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books"
-        );
-        setBooks(response.data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    getBooks();
-  }, []);
-
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error: {error}</div>;
 
   return (
     <div>
       <h1>Book List</h1>
       <ul>
         {books.map((book, index) => (
-          <li key={index}>{book.title}</li>
+          <li key={index}>
+            {book.title}
+            <button onClick={() => navigate(`/book/${book.id}`)}>
+              More Book Information
+            </button>
+          </li>
         ))}
       </ul>
     </div>
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,1 +1,25 @@
 /* TODO - add your code to create a functional React component that renders details for a single book. Fetch the book data from the provided API. You may consider conditionally rendering a 'Checkout' button for logged in users. */
+import { useGetBookByIdQuery } from "./apiSlice.js";
+import { useParams } from "react";
+
+const singleBook = () => {
+  const { id } = useParams();
+  const { data } = useGetBookByIdQuery();
+  const book = data.book;
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error: {error}</div>;
+
+  return (
+    <>
+      <img src={book.coverimage} />
+      <br></br>
+      <h3>{book.available ? "yes" : "no"}</h3>
+      <h2>{book.title}</h2>
+      <h2> {book.author}</h2>
+      <h3>{book.description}</h3>
+      <br></br>
+      <button onClick={() => navigate(`/book/${book.id}`)}>Checkout</button>
+    </>
+  );
+};
