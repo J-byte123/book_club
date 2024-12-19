0b6d51d25c48bda7fd0c17e8e043fc6ec48b36cc
Nicole Riera <150852807+NicoRi5@users.noreply.github.com>
2024-12-18 20:56:03 -0500

    Implement redux

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,55 +1,41 @@
 /* TODO - add your code to create a functional React component that renders account details for a logged in user. Fetch the account data from the provided API. You may consider conditionally rendering a message for other users that prompts them to log in or create an account.  */
 
-import { useEffect, useState } from "react";
-// possible import:
-// import { Link } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+// CHANGES MADE:
+// - remove import for useEffect and useState
+// instead use apiSlice and Query
+// - instead of fetch, useGetBooksQuery has been implemented in order to fetch the users checked out books
+import { useGetBooksQuery } from "../Slice/apiSlice";
+import { useLocation, Link } from "react-router-dom";
 
-// removed setToken, setError
-// new: const state for the useLocation import (brings in information)
 export default function Account() {
-  const [books, setBooks] = useState("");
+  const { data: books = [] } = useGetBooksQuery(user.id);
+
   const { state } = useLocation();
   //  the following brings in the logged-in users info from login.jsx file
   const user = state.user;
 
-  useEffect(() => {
-    const fetchBooks = async () => {
-      try {
-        const response = await fetch(
-          `https://fsa-book-buddy-b6e748d1380d.herokuapp.com/docs/${user.id}`
-        );
-
-        if (response.ok) {
-          const data = await response.json();
-          setBooks(data);
-        } else {
-          setBooks([]);
-        }
-      } catch (error) {
-        setBooks([]);
-      }
-    };
-    fetchBooks();
-  }, [user]);
-
   return (
     <div>
-      <h4>Welcome {user.email}</h4>
-      <br>
-        <h2>Checked Out Books:</h2>
-        {books.length > 0 ? (
-          <ul>
-            {books.map((book) => (
-              <li key={book.id}>
-                {book.title} by {book.author}
-              </li>
-            ))}
-          </ul>
-        ) : (
+      <h4>Welcome, {user.email}</h4>
+
+      <h2>Checked Out Books:</h2>
+      {books.length > 0 ? (
+        <ul>
+          {books.map((book) => (
+            <li key={book.id}>
+              {book.title} by {book.author}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>
           <p>No books have been checked out</p>
-        )}
-      </br>
+          <p>
+            {/* NEW: Link to books.jsx so users can view catalog */}
+            <Link to="/books">Explore our book catalog!</Link>
+          </p>
+        </div>
+      )}
     </div>
   );
 }
