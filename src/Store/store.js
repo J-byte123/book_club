cecac7bf2ac2f5dc12bbaf8d6c32b0d2aa0bc9c1
EmSmith-Navy <keino2586@gmail.com>
2024-12-29 17:58:16 -0600

    authSlice

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,12 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { apiSlice } from './src/Slices/apiSlice';
-import authReducer from './src/Slices/authSlice';
-const store = configureStore({
+import authReducer from './authSlice';
+// src/index.js
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+// eslint-disable-next-line no-unused-vars
+import App from './App';
+
+import SingleBook from '../components/SingleBook';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <SingleBook />
+  </Provider>
+);
+
+export default configureStore({
   reducer: {
     auth: authReducer,
-    [apiSlice.reducerPath]: apiSlice.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
 });
-export default store;
